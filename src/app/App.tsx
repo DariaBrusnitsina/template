dc6a1f391f3c2312b8e904c44f899c149d45609d
DariaBrusnitsina <dbrusnitsina@gmail.com>
2023-11-17 19:05:51 +0300

    feat: implement navbar

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import './styles/index.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+
+export const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <button onClick={toggleTheme}>Toggle</button>
+            <AppRouter />
+        </div>
+    );
+};
